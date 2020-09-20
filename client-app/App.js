08705cc2ce9a7d2d9bdd9e4bb23e168b7f001408
Sangwon <jackoss@naver.com>
2020-09-20 22:54:53 +0900

    #10.2 Preloading Cache

--- client-app/App.js
+++ client-app/App.js
@@ -1,17 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
-import { AppLoading, Font, Asset } from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import { ApolloClient, ApolloProvider } from "@apollo/client"
+
+import { Clipboard } from 'react-native'
+
+if (__DEV__) {
+  Clipboard.setString('')
+}
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+
   const preLoad = async () => {
     try {
       await Font.loadAsync({
         ...Ionicons.font
       });
-      await Asset.loadAsync([require("./assets/logo.png")]);
+      await Asset.loadAsync([require("./assets/logo.png")]);//App이 필요한 이미지를 미리 upload, 로고같은 것
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      })
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -21,10 +44,12 @@ export default function App() {
     preLoad();
   }, [])
 
-  return loaded ? (
-    <View>
-      <Text>ihiihihi</Text>
-    </View>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>ihiihihi</Text>
+      </View>
+    </ApolloProvider>
   ) : (
       <AppLoading />
     )
