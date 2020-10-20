f44ae3eb963a92da531c51e6a7484cf2e625e5a8
Sangwon <jackoss@naver.com>
2020-10-21 01:26:05 +0900

    #4.3 Apollo Client

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+import Client from "./Apollo/Client";
+import App from "./Components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={Client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
